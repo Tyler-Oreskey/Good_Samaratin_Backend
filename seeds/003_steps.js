3e8fe84978aa9bd7d30bdaba07984645341cfd89
Tyler <toreskey@gmail.com>
2018-12-12 14:12:18 -0700

    changed some seed names

--- seeds/003_steps.js
+++ seeds/003_steps.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('procedures').del()
+  return knex('steps').del()
     .then(function () {
       // Inserts seed entries
-      return knex('procedures').insert([
+      return knex('steps').insert([
         {id: 1, emergency_name: ''},
         {id: 1, procedure: 'rowValue1'},
         {id: 1, procedure: 'rowValue1'},
@@ -13,7 +13,7 @@ exports.seed = function(knex, Promise) {
       ]);
       .then(function() {
          //Moves id column (PK) auto-incremented to correct value after inserts
-        return knex.raw(`SELECT setval(`procedures_id_seq`, (SELECT MAX(id) FROM procedures))`)
+        return knex.raw(`SELECT setval(`steps_id_seq`, (SELECT MAX(id) FROM steps))`)
       })
     });
 };
